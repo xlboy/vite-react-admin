1ea9f451b745330eaaba7b73312f9c3e6ef305cd
xlboy <984492408@qq.com>
2021-10-09 22:23:25 +0800

    feat: ✨ 完善tagsView的store层

--- src/store/modules/system.ts
+++ src/store/modules/system.ts
@@ -5,16 +5,16 @@ import { createSlice } from '@reduxjs/toolkit';
 
 interface SystemState {
   locale: LocaleTypes;
-  cacheTags: any[];
-  activeTag: any;
+  cacheTagIds: string[];
+  activeTagId: null | string;
   isMobile: boolean;
   isMenuCollapsed: boolean;
 }
 
 const initialState: SystemState = {
   locale: (localStorage.getItem(appConfig.cacheKey.locale) ?? 'zh-CN') as LocaleTypes,
-  cacheTags: [],
-  activeTag: null,
+  cacheTagIds: [],
+  activeTagId: null,
   isMobile: false,
   isMenuCollapsed: false
 };
@@ -24,13 +24,49 @@ const systemSlice = createSlice({
   initialState,
   reducers: {
     setIsMenuCollapsed(state, action: PayloadAction<undefined | boolean>) {
-      state.isMenuCollapsed = action.payload ?? !state.isMenuCollapsed;
+      const isMenuCollapsed = action.payload;
+
+      state.isMenuCollapsed = isMenuCollapsed ?? !state.isMenuCollapsed;
     },
     setIsMobile(state, action: PayloadAction<boolean>) {
       state.isMobile = action.payload;
     },
     setLocale(state, action: PayloadAction<LocaleTypes>) {
-      state.locale = action.payload;
+      const locale = action.payload;
+
+      state.locale = locale;
+    },
+    removeAllTag(state) {
+      state.cacheTagIds = [];
+    },
+    removeExceptSpecifiedTagIds(state, action: PayloadAction<string>) {
+      const specifiedTagId = action.payload;
+
+      state.cacheTagIds = [specifiedTagId];
+      state.activeTagId = specifiedTagId;
+    },
+    removeSpecifiedTagId(state, action: PayloadAction<string>) {
+      const specifiedTagId = action.payload;
+      const tagIdIndex = state.cacheTagIds.indexOf(specifiedTagId);
+      const isFirstOne = tagIdIndex === 0;
+      const isLast = tagIdIndex === state.cacheTagIds.length - 1;
+
+      state.cacheTagIds.splice(tagIdIndex, 1);
+
+      if (state.cacheTagIds.length !== 0) {
+        // 刚好要删除的是第一个，则将tag切换到最新一位
+        if (isFirstOne) {
+          state.activeTagId = state.cacheTagIds[0];
+        } else if (isLast) {
+          // 以此类推，删的是最后一个，切换至最后一位
+          state.activeTagId = state.cacheTagIds.at(-1)!;
+        }
+      }
+    },
+    setActiveTagId(state, action: PayloadAction<string>) {
+      const activeTagId = action.payload;
+
+      state.activeTagId = activeTagId;
     }
   }
 });
