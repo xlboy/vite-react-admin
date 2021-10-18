421826bcb6a894fc3a271c904fbf7643028c5b0c
xlboy <984492408@qq.com>
2021-10-18 13:36:26 +0800

    docs: 📃 添加国际化的介绍

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,28 +1,29 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "vite-react-admin",
-  tagline: "一个基于 Vite、TypeScript、React、AntD 搭建的后台管理系统模板，它拥有极致的 TS 开发体验与内置有多种特性，相信它能给你带来一定的收获。",
-  url: "https://your-docusaurus-test-site.com",
-  baseUrl: "/",
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  title: 'vite-react-admin',
+  tagline:
+    '一个基于 Vite、TypeScript、React、AntD 搭建的后台管理系统模板，它拥有极致的 TS 开发体验与内置有多种特性，相信它能给你带来一定的收获。',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
 
   presets: [
     [
-      "@docusaurus/preset-classic",
+      '@docusaurus/preset-classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
+          sidebarPath: require.resolve('./sidebars.js')
           // Please change this to your repo.
           // editUrl: "https://github.com/facebook/docusaurus/edit/main/website/",
         },
@@ -33,37 +34,37 @@ const config = {
         //     "https://github.com/facebook/docusaurus/edit/main/website/blog/",
         // },
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
-        },
-      }),
-    ],
+          customCss: require.resolve('./src/css/custom.css')
+        }
+      })
+    ]
   ],
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: "vite-react-admin",
+        title: 'vite-react-admin',
         logo: {
-          alt: "Logo",
-          src: "img/logo.svg",
+          alt: 'Logo',
+          src: 'img/logo.svg'
         },
         items: [
           {
-            type: "doc",
-            docId: "guide/introduction",
-            position: "left",
-            label: "指南",
+            type: 'doc',
+            docId: 'guide/introduction',
+            position: 'left',
+            label: '指南'
           },
           {
-            type: "doc",
-            docId: "tool-chain/index",
-            position: "right",
-            label: "工具链",
+            type: 'doc',
+            docId: 'tool-chain/index',
+            position: 'right',
+            label: '工具链'
           },
           {
-            type: "localeDropdown",
-            position: "right",
+            type: 'localeDropdown',
+            position: 'right'
           },
           // {
           //   type: "doc",
@@ -73,17 +74,17 @@ const config = {
           // },
           // { to: "/blog", label: "Blog", position: "left" },
           {
-            href: "https://github.com/xlboy/vite-react-admin",
-            label: "GitHub",
-            position: "right",
-          },
-        ],
+            href: 'https://github.com/xlboy/vite-react-admin',
+            label: 'GitHub',
+            position: 'right'
+          }
+        ]
       },
       prism: {
         theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
+        darkTheme: darkCodeTheme
+      }
+    })
 };
 
 module.exports = config;
