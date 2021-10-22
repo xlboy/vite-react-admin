1c4b8aacf4d059cf7db7d3c0fa4f1c48bbcfdc13
xlboy <984492408@qq.com>
2021-10-22 18:35:17 +0800

    feat: ✨ login页的部分实现

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,19 +1,45 @@
-import { UserService } from '@/services';
+import SwitchLanguage from '@/layout/SwitchLanguage';
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Form, Input } from 'antd';
+import Checkbox from 'antd/lib/checkbox/Checkbox';
+import './index.less';
+import { ReactComponent as IconAntd } from '@/assets/icons/icon-antd.svg';
 
 interface LoginPageProps {}
 
-const LoginPage: React.FC<LoginPageProps> = props => {
-  return (
-    <div>
-      <button
-        onClick={async () => {
-          const res = await UserService.login({ username: '', password: '' });
+const LoginPage: React.FC<LoginPageProps> = () => {
+  const handleFormSubmit = () => {
+    console.log('最怕只剩，自己不没有你');
+  };
 
-          console.log('res', res);
-        }}
-      >
-        请求示例
-      </button>
+  return (
+    <div className="login-page">
+      <div className="locale-switch">
+        <SwitchLanguage />
+      </div>
+      <div className="login-model">
+        <div className="login-model-logo">
+          <IconAntd className="login-model-logo__icon" />
+        </div>
+        <Form onFinish={handleFormSubmit}>
+          <Form.Item name="username" rules={[{ required: true, message: 'Please input your Username!' }]}>
+            <Input prefix={<UserOutlined />} />
+          </Form.Item>
+          <Form.Item name="password" rules={[{ required: true, message: 'Please input your Password!' }]}>
+            <Input prefix={<LockOutlined />} type="password" />
+          </Form.Item>
+          <Form.Item>
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>记住密码</Checkbox>
+            </Form.Item>
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" block htmlType="submit">
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
     </div>
   );
 };
